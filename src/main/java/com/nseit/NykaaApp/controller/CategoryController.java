e29bad87c985653821cbde43c41b4078c32a8736
GeeksforGeeks <srinidhimukunthan@gmail.orgg>
2022-08-26 21:42:40 +0530

    initial commit

--- src/main/java/com/nseit/NykaaApp/controller/CategoryController.java
+++ src/main/java/com/nseit/NykaaApp/controller/CategoryController.java
@@ -1,13 +1,21 @@
 package com.nseit.NykaaApp.controller;
 
+import com.nseit.NykaaApp.model.Category;
+import com.nseit.NykaaApp.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/Category")
 public class CategoryController {
-    public void addCategory(){
-
+    @Autowired
+    private CategoryService categoryService;
+    @PostMapping
+    public void addCategory(@RequestBody Category category){
+        categoryService.addCategory(category);
     }
     public void viewCategory(){
 
--- src/main/java/com/nseit/NykaaApp/service/CategoryService.java
+++ src/main/java/com/nseit/NykaaApp/service/CategoryService.java
@@ -6,27 +6,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
-import static com.sun.org.apache.xerces.internal.impl.xpath.regex.Token.categories;
-
 @Service
 public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
-    public void addCategory(Category category){
+    public void addCategory(Category category) {
         categoryRepository.save(category);
     }
-    public List<Category> viewCategory(){
+
+    public List<Category> viewCategory() {
         return categoryRepository.findAll();
     }
-    public void updateCategory(Category category){
+
+    public void updateCategory(Category category) {
         categoryRepository.save(category);
     }
-    public void deleteCategory(int categoryId){
-        List<Category>categories=categoryRepository.findAll();
-        for(Category category: categories);
-        if(categoryId == category.getId());
+
+    public void deleteCategory(int categoryId) {
+
     }
 
 }
